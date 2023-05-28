a61527b28e4d4cde5da613d7a86df3f04cfef95a
TeraJP <joaofarias.tera@gmail.com>
2023-05-27 23:19:08 -0300

    loucura

--- script.js
+++ script.js
@@ -1,120 +1,138 @@
+function toggleCategory(categoryId) {
+  var categoryContent = document.getElementById(categoryId);
+  categoryContent.style.display = categoryContent.style.display === 'block' ? 'none' : 'block';
+}
 
-  function toggleCategory(categoryId) {
-    var categoryContent = document.getElementById(categoryId);
-    categoryContent.style.display = categoryContent.style.display === 'block' ? 'none' : 'block';
-  }
+var availability = {
+  Mandacaru: true,
+  "X-Tudo": true,
+  Arretado: true,
+  Bacon: true
+};
 
 document.addEventListener("DOMContentLoaded", function() {
+  var cartItems = [];
 
+  var addToCartButtons = document.getElementsByClassName("add-to-cart");
+  for (var i = 0; i < addToCartButtons.length; i++) {
+    var button = addToCartButtons[i];
+    var name = button.getAttribute("data-name");
+    var price = parseFloat(button.getAttribute("data-price"));
+    var available = availability[name] || false;
 
-
-    var cartItems = [];
-  
-    var addToCartButtons = document.getElementsByClassName("add-to-cart");
-    for (var i = 0; i < addToCartButtons.length; i++) {
-      addToCartButtons[i].addEventListener("click", function() {
-        var name = this.getAttribute("data-name");
-        var price = parseFloat(this.getAttribute("data-price"));
-  
-        var item = {
-          name: name,
-          price: price,
-          quantity: 1
-        };
-  
-        var existingItem = cartItems.find(function(cartItem) {
-          return cartItem.name === name;
-        });
-  
-        if (existingItem) {
-          existingItem.quantity++;
-        } else {
-          cartItems.push(item);
-        }
-  
-        updateCart();
-        showSuccessMessage('Item adicionado ao carrinho!');
-      });
+    if (!available) {
+      button.disabled = true;
+      button.innerText = "Indisponível";
+      button.classList.add("btn-secondary");
     }
-  
-    var checkoutButton = document.getElementById("checkout-btn");
-    checkoutButton.addEventListener("click", function() {
-      var totalPrice = cartItems.reduce(function(total, cartItem) {
-        return total + (cartItem.price * cartItem.quantity);
-      }, 0);
-  
-      var message = 'Resumo do Pedido:\n\n';
-      cartItems.forEach(function(cartItem) {
-        message += 'Item: ' + cartItem.name + '\n';
-        message += 'Quantidade: ' + cartItem.quantity + '\n';
-        message += 'Subtotal: R$ ' + (cartItem.price * cartItem.quantity).toFixed(2) + '\n\n';
+
+    button.addEventListener("click", function() {
+      var name = this.getAttribute("data-name");
+      var price = parseFloat(this.getAttribute("data-price"));
+      var available = availability[name] || false;
+
+      if (!available) {
+        return; // Sai da função e não adiciona o item ao carrinho
+      }
+
+      var item = {
+        name: name,
+        price: price,
+        quantity: 1,
+        available: available
+      };
+
+      var existingItem = cartItems.find(function(cartItem) {
+        return cartItem.name === name;
       });
-      message += 'Total: R$ ' + totalPrice.toFixed(2);
-  
-      sessionStorage.setItem('checkoutMessage', message);
-      window.location.href = 'summary.html';
-  
-      cartItems = [];
+
+      if (existingItem) {
+        existingItem.quantity++;
+      } else {
+        cartItems.push(item);
+      }
+
       updateCart();
+      showSuccessMessage('Item adicionado ao carrinho!');
     });
-  
-    function updateCart() {
-      var tableBody = document.getElementById("cart").getElementsByTagName("tbody")[0];
-      tableBody.innerHTML = "";
-  
-      var cartTotal = 0;
-  
-      cartItems.forEach(function(cartItem) {
-        var total = cartItem.price * cartItem.quantity;
-        cartTotal += total;
-  
-        var row = "<tr>";
-        row += '<td class="cart-item-name">' + cartItem.name + "</td>";
-        row += "<td>R$ " + cartItem.price.toFixed(2) + "</td>";
-        row += "<td>" + cartItem.quantity + "</td>";
-        row += '<td><button class="btn btn-danger btn-sm remove-item" data-name="' + cartItem.name + '">Remover</button></td>';
-        row += "</tr>";
-  
-        tableBody.innerHTML += row;
-      });
-  
-      document.getElementById("cart-total").textContent = "R$ " + cartTotal.toFixed(2);
-  
-      var removeButtons = document.getElementsByClassName("remove-item");
-      for (var i = 0; i < removeButtons.length; i++) {
-        removeButtons[i].addEventListener("click", function() {
-          var name = this.getAttribute("data-name");
-          cartItems = cartItems.filter(function(cartItem) {
-            return cartItem.name !== name;
-          });
-  
-          updateCart();
-          showSuccessMessage("Item removido do carrinho.");
+  }
+
+  var checkoutButton = document.getElementById("checkout-btn");
+  checkoutButton.addEventListener("click", function() {
+    var totalPrice = cartItems.reduce(function(total, cartItem) {
+      return total + cartItem.price * cartItem.quantity;
+    }, 0);
+
+    var message = 'Resumo do Pedido:\n\n';
+    cartItems.forEach(function(cartItem) {
+      message += 'Item: ' + cartItem.name + '\n';
+      message += 'Quantidade: ' + cartItem.quantity + '\n';
+      message += 'Subtotal: R$ ' + (cartItem.price * cartItem.quantity).toFixed(2) + '\n\n';
+    });
+    message += 'Total: R$ ' + totalPrice.toFixed(2);
+
+    sessionStorage.setItem('checkoutMessage', message);
+    window.location.href = 'summary.html';
+
+    cartItems = [];
+    updateCart();
+  });
+
+  function updateCart() {
+    var tableBody = document.getElementById("cart").getElementsByTagName("tbody")[0];
+    tableBody.innerHTML = "";
+
+    var cartTotal = 0;
+
+    cartItems.forEach(function(cartItem) {
+      var total = cartItem.price * cartItem.quantity;
+      cartTotal += total;
+
+      var row = "<tr>";
+      row += '<td class="cart-item-name">' + cartItem.name + "</td>";
+      row += "<td>R$ " + cartItem.price.toFixed(2) + "</td>";
+      row += "<td>" + cartItem.quantity + "</td>";
+      row += '<td><button class="btn btn-danger btn-sm remove-item" data-name="' + cartItem.name + '">Remover</button></td>';
+      row += "</tr>";
+
+      tableBody.innerHTML += row;
+    });
+
+    document.getElementById("cart-total").textContent = "R$ " + cartTotal.toFixed(2);
+
+    var removeButtons = document.getElementsByClassName("remove-item");
+    for (var i = 0; i < removeButtons.length; i++) {
+      removeButtons[i].addEventListener("click", function() {
+        var name = this.getAttribute("data-name");
+        cartItems = cartItems.filter(function(cartItem) {
+          return cartItem.name !== name;
         });
-      }
-    }
-  
-    function showSuccessMessage(message) {
-      Swal.fire({
-        icon: "success",
-        title: "Sucesso!",
-        text: message,
-        showConfirmButton: false,
-        timer: 2000,
-        toast: true,
-        position: 'top-end'
+
+        updateCart();
+        showSuccessMessage("Item removido do carrinho.");
       });
     }
-  });
-  
-  document.addEventListener('DOMContentLoaded', function() {
-    var navbar = document.querySelector('.navbar');
-    var navbarCollapse = document.querySelector('.navbar-collapse');
-  
-    document.addEventListener('click', function(event) {
-      var target = event.target;
-      if (!navbar.contains(target)) {
-        navbarCollapse.classList.remove('show');
-      }
+  }
+
+  function showSuccessMessage(message) {
+    Swal.fire({
+      icon: "success",
+      title: "Sucesso!",
+      text: message,
+      showConfirmButton: false,
+      timer: 2000,
+      toast: true,
+      position: 'top'
     });
+  }
+
+  var navbar = document.querySelector('.navbar');
+  var navbarCollapse = document.querySelector('.navbar-collapse');
+
+  document.addEventListener('click', function(event) {
+    var target = event.target;
+    if (!navbar.contains(target)) {
+      navbarCollapse.classList.remove('show');
+    }
   });
+});
--- scriptSummary.js
+++ scriptSummary.js
@@ -1,21 +1,55 @@
 var whatsappButton = document.getElementById("whatsappButton");
-whatsappButton.addEventListener("click", function() {
-
+whatsappButton.addEventListener("click", function () {
   var nameInput = document.getElementById("name");
   var addressInput = document.getElementById("address");
+  var paymentMethodSelect = document.getElementById("payment-method");
+  var pixFields = document.getElementById("pix-fields");
   var moneyChangeInput = document.getElementById("money-change");
 
   var name = nameInput.value;
   var address = addressInput.value;
-  var moneyChange = moneyChangeInput.value;
+  var paymentMethod = paymentMethodSelect.value;
+  var moneyChange = '';
 
-  var message = sessionStorage.getItem('checkoutMessage');
-  var finalMessage = message + '\n\n';
-  finalMessage += 'Nome: ' + name + '\n';
-  finalMessage += 'Endereço: ' + address + '\n';
-  finalMessage += 'Troco para quanto: ' + moneyChange;
+  if (paymentMethod === 'dinheiro') {
+    moneyChange = moneyChangeInput.value;
+  }
 
-  var url = 'https://api.whatsapp.com/send?phone=+55798116-9700&text=' + encodeURIComponent(finalMessage);
-  window.open(url, '_blank');
+  if (name && address && paymentMethod && (paymentMethod !== 'dinheiro' || (paymentMethod === 'dinheiro' && moneyChange))) {
+    var message = sessionStorage.getItem('checkoutMessage');
+    var finalMessage = message + '\n\n';
+    finalMessage += 'Nome: ' + name + '\n';
+    finalMessage += 'Endereço: ' + address + '\n';
+    finalMessage += 'Forma de Pagamento: ' + paymentMethod + '';
 
+    if (paymentMethod === 'dinheiro') {
+      finalMessage += '\nTroco para quanto: ' + moneyChange;
+    }
+
+    if(paymentMethod === 'pix') {
+      finalMessage += ': (79) 99879-4942 / João Pedro, Will Pagamentos'
+    }
+
+    var url = 'https://api.whatsapp.com/send?phone=+55798116-9700&text=' + encodeURIComponent(finalMessage);
+    window.open(url, '_blank');
+  } else {
+    alert("Por favor, preencha todos os campos.");
+  }
 });
+
+function togglePaymentFields() {
+  var paymentMethodSelect = document.getElementById("payment-method");
+  var pixFields = document.getElementById("pix-fields");
+  var moneyChangeContainer = document.getElementById("money-change-container");
+
+  if (paymentMethodSelect.value === "pix") {
+    pixFields.style.display = "block";
+    moneyChangeContainer.style.display = "none";
+  } else if (paymentMethodSelect.value === "dinheiro") {
+    pixFields.style.display = "none";
+    moneyChangeContainer.style.display = "block";
+  } else {
+    pixFields.style.display = "none";
+    moneyChangeContainer.style.display = "none";
+  }
+}
